23fb7d26cc8d906839d7c209b7db20d82397c6ab
tredwood89 <tredwood89@gmail.com>
2017-11-04 00:28:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function sayHiToGrandma(string) {
   } else if (string === string.toLowerCase()) {
       return "I can't hear you!"
     } else if (string = "I love you, Grandma.") {
-        return "I love you, too." 
+        return "I love you, too."
     }
 }
